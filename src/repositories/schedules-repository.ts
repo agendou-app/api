d2d9d0df7de5f724c24a56334395b156b7420c36
kaikySantos <carlos.kaiky62@gmail.com>
2024-11-23 16:06:58 -0300

    feat(schedules): update schedule use case

--- src/repositories/schedules-repository.ts
+++ src/repositories/schedules-repository.ts
@@ -1,6 +1,7 @@
 import { Prisma, Schedule } from '@prisma/client'
 
 export interface SchedulesRepository {
+  findById(id: string): Promise<Schedule | null>
   findBySlug(slug: string): Promise<Schedule | null>
   create(data: Prisma.ScheduleCreateInput): Promise<Schedule>
   update(id: string, data: Prisma.ScheduleUpdateInput): Promise<Schedule>
---
+++ src/use-cases/schedules/update.ts
@@ -0,0 +1,82 @@
+import { Prisma, Schedule } from "@prisma/client"
+import { ResourceNotFoundError } from "@/errors/resource-not-found"
+import { ScheduleSameSlugError } from "@/errors/schedule-same-slug"
+import { SchedulesRepository } from "@/repositories/schedules-repository"
+
+interface Request {
+  id: string
+  name?: string
+  about?: string
+  slug?: string
+  logoUrl?: string
+  contact?: Prisma.ContactCreateWithoutScheduleInput
+  address?: Prisma.AddressCreateWithoutScheduleInput
+}
+
+interface Response {
+  schedule: Schedule
+}
+
+export class UpdateScheduleUseCase {
+  constructor (private schedulesRepository: SchedulesRepository) {}
+
+  async execute({
+    id,
+    name,
+    about,
+    slug,
+    logoUrl,
+    contact,
+    address
+  }: Request): Promise<Response> {
+    const scheduleExists = await this.schedulesRepository.findById(id)
+
+    if (!scheduleExists) {
+      throw new ResourceNotFoundError()
+    }
+
+    if (slug) {
+      const scheduleWithSameSlug = await this.schedulesRepository.findBySlug(slug)
+
+      if (scheduleWithSameSlug) {
+        throw new ScheduleSameSlugError()
+      }
+    }
+
+    const schedule = await this.schedulesRepository.update(id, {
+      name,
+      about,
+      slug,
+      logoUrl,
+      contact: {
+        update: {
+          where: {
+            id: contact?.id
+          },
+          data: {
+            phone: contact?.phone,
+            email: contact?.email
+          }
+        }
+      },
+      address: {
+        update: {
+          where: {
+            id: address?.id
+          },
+          data: {
+            street: address?.street,
+            number: address?.number,
+            neighborhood: address?.neighborhood,
+            city: address?.city,
+            state: address?.state,
+            zipCode: address?.zipCode,
+            complement: address?.complement,
+          }
+        }
+      }
+    })
+
+    return { schedule }
+  }
+}
