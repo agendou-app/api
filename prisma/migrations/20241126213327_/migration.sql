89d2c0f0661949ccadaba651cd8796d89871cbf0
kaikySantos <carlos.kaiky62@gmail.com>
2024-11-26 18:33:39 -0300

    feat: repositório de schedules

--- prisma/migrations/20241126213327_/migration.sql
+++ prisma/migrations/20241126213327_/migration.sql
@@ -1,3 +1,18 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('ADMINISTRATOR', 'USER');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password_hash" TEXT NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'USER',
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
 -- CreateTable
 CREATE TABLE "schedules" (
     "id" TEXT NOT NULL,
@@ -7,6 +22,7 @@ CREATE TABLE "schedules" (
     "slug" TEXT NOT NULL,
     "contactId" TEXT NOT NULL,
     "addressId" TEXT,
+    "userId" TEXT,
 
     CONSTRAINT "schedules_pkey" PRIMARY KEY ("id")
 );
@@ -35,6 +51,9 @@ CREATE TABLE "address" (
 );
 
 -- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
 CREATE UNIQUE INDEX "schedules_slug_key" ON "schedules"("slug");
 
 -- CreateIndex
@@ -45,3 +64,6 @@ ALTER TABLE "schedules" ADD CONSTRAINT "schedules_contactId_fkey" FOREIGN KEY ("
 
 -- AddForeignKey
 ALTER TABLE "schedules" ADD CONSTRAINT "schedules_addressId_fkey" FOREIGN KEY ("addressId") REFERENCES "address"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "schedules" ADD CONSTRAINT "schedules_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE SET NULL ON UPDATE CASCADE;
---
+++ src/repositories/prisma/prisma-schedules-repository.ts
@@ -0,0 +1,77 @@
+import { Prisma } from '@prisma/client'
+import { SchedulesRepository } from '../schedules-repository'
+import { prisma } from '@/lib/prisma'
+
+export class PrismaSchedulesRepository implements SchedulesRepository {
+  async findById(id: string) {
+    const schedule = await prisma.schedule.findUnique({
+      where: {
+        id,
+      },
+    })
+
+    return schedule
+  }
+
+  async findByIdAndUserId(id: string, userId: string) {
+    const schedule = await prisma.schedule.findUnique({
+      where: {
+        id,
+        userId,
+      },
+    })
+
+    return schedule
+  }
+
+  async findBySlug(slug: string) {
+    const schedule = await prisma.schedule.findUnique({
+      where: {
+        slug,
+      },
+    })
+
+    return schedule
+  }
+
+  async findAllByUser(userId: string) {
+    const schedules = await prisma.schedule.findMany({
+      where: {
+        userId,
+      },
+      include: {
+        contact: true,
+        address: true,
+      },
+    })
+
+    return schedules
+  }
+
+  async create(data: Prisma.ScheduleCreateInput) {
+    const schedule = await prisma.schedule.create({
+      data,
+    })
+
+    return schedule
+  }
+
+  async update(id: string, data: Prisma.ScheduleUpdateInput) {
+    const schedule = await prisma.schedule.update({
+      where: {
+        id,
+      },
+      data,
+    })
+
+    return schedule
+  }
+
+  async remove(id: string) {
+    await prisma.schedule.delete({
+      where: {
+        id,
+      },
+    })
+  }
+}
